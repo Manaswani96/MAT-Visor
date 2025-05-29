7426aa4e3999e41fcc818d06764377f26e023a16
Mahi.K <katikelamanaswani9969p@gmail.com>
2025-05-29 19:52:36 +0530

    Create create_gif.m

---
+++ create_gif.m
@@ -0,0 +1,24 @@
+% create_gif.m
+% Capture current MATLAB figure as animated GIF
+
+function create_gif(filename, nFrames, delay)
+% filename - output GIF file name (string)
+% nFrames - number of frames in the GIF
+% delay - delay time between frames (seconds)
+
+if nargin < 3
+    delay = 0.1; % default delay
+end
+
+for k = 1:nFrames
+    frame = getframe(gcf);
+    img = frame2im(frame);
+    [imind, cm] = rgb2ind(img, 256);
+
+    if k == 1
+        imwrite(imind, cm, filename, 'gif', 'Loopcount', inf, 'DelayTime', delay);
+    else
+        imwrite(imind, cm, filename, 'gif', 'WriteMode', 'append', 'DelayTime', delay);
+    end
+end
+end
